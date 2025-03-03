eb1d05a0232dec4a43aadf198ded5c5529fd44d6
Oural <me@oural.dev>
2025-03-03 21:06:23 +0100

    Update production reader style with content-style.sass

--- components/HomeView/Productions.vue
+++ components/HomeView/Productions.vue
@@ -1,7 +1,17 @@
 <script setup lang="ts">
 const img = useImage()
 const { data: productions } = await useAsyncData(() => queryCollection('productions').all())
+
 const currentProduction: any = ref(null)
+
+watch(currentProduction, (newValue) => {
+  if (newValue) {
+    document.body.classList.add('no-scroll')
+  }
+  else {
+    document.body.classList.remove('no-scroll')
+  }
+})
 </script>
 
 <template>
@@ -16,12 +26,11 @@ const currentProduction: any = ref(null)
             <p v-html="production.description" />
           </div>
           <ul class="tags">
-            <li v-for="(tag, tagIndex) in production.meta.competences" :key="tagIndex">
+            <li v-for="(tag, tagIndex) in production.meta.competences.slice(0, 3)" :key="tagIndex">
               <NuxtImg :src="`/assets/skills-icons/${tag.icon}`" :alt="`Icon de ${tag.name}`" />
               <span>{{ tag.name }}</span>
             </li>
           </ul>
-          <NuxtImg class="expand" src="/assets/icons/cross.svg" alt="Icon plus" />
         </div>
       </div>
     </div>
@@ -29,6 +38,13 @@ const currentProduction: any = ref(null)
       <div class="background" @click="currentProduction = null" />
       <div class="container">
         <NuxtImg class="close-btn" src="/assets/icons/cross.svg" alt="Icon fermer" @click="currentProduction = null" />
+        <h1>{{ currentProduction.title }}</h1>
+        <ul class="tags">
+          <li v-for="(tag, tagIndex) in currentProduction?.meta.competences" :key="tagIndex">
+            <NuxtImg :src="`/assets/skills-icons/${tag.icon}`" :alt="`Icon de ${tag.name}`" />
+            <span>{{ tag.name }}</span>
+          </li>
+        </ul>
         <ContentRenderer class="content" :value="currentProduction" />
       </div>
     </div>
@@ -37,6 +53,11 @@ const currentProduction: any = ref(null)
 
 <style lang="sass">
 @use "/assets/style/_variables" as *
+@use "/assets/style/content-style" as *
+
+body.no-scroll
+  height: 100%
+  overflow-y: hidden
 
 #réalisations
   width: 100%
@@ -45,7 +66,7 @@ const currentProduction: any = ref(null)
   display: flex
   flex-direction: column
   @include padding
-  h2
+  > h2
     font-weight: 900
     font-size: 1.625rem
     margin-bottom: 7.5rem
@@ -53,7 +74,7 @@ const currentProduction: any = ref(null)
     span
       margin-left: 0.5rem
       font-size: 2rem
-  .container
+  > .container
     display: flex
     justify-content: center
     flex-wrap: wrap
@@ -75,8 +96,7 @@ const currentProduction: any = ref(null)
         background-size: cover
         border-radius: 0.75rem
       .content
-        position: relative
-        padding: 1rem
+        padding: 1rem 1.5rem 1rem 1rem
         display: flex
         flex-direction: column
         justify-content: space-between
@@ -89,37 +109,9 @@ const currentProduction: any = ref(null)
           p
             font-size: 1rem
             line-height: 1.375
-        .tags
-          display: flex
-          gap: 0.5rem
-          li
-            display: flex
-            gap: 0.25rem
-            align-items: center
-            background-color: $color-secondary
-            padding: 0.25rem
-            border-radius: 0.25rem
-            img
-              filter: $filter-primary
-              width: 1rem
-              height: 1rem
-            span
-              color: $color-primary
-              font-size: 0.825rem
-              font-weight: 800
-        .expand
-          position: absolute
-          bottom: 1.25rem
-          right: 1rem
-          width: 1rem
-          height: 1rem
-          filter: $filter-secondary
-          transform: rotate(45deg)
       &:hover
         transform: scale(1.0125)
         box-shadow: 0 0 2rem rgba(0, 70, 67, 0.75)
-        .expand
-          filter: $filter-highlight
     @media (max-width: $m)
       gap: 3.5rem
       .production
@@ -131,21 +123,29 @@ const currentProduction: any = ref(null)
   .reader
     .container
       z-index: 15
+      display: flex
+      align-items: center
+      flex-direction: column
       position: fixed
       top: 50%
       left: 50%
       transform: translate(-50%, -50%)
       padding: 3rem
-      border-radius: 5px
-      width: 50vw
+      border-radius: 0.75rem
+      width: 62.5vw
+      max-height: 95vh
       color: $color-secondary
       background-color: $color-primary
-      .content
-        width: 100%
-        height: 100%
-        text-align: left
-        h1, h2
-          color: $color-white
+      > h1
+        font-size: 2rem
+        color: $color-white
+        font-weight: 800
+        margin-bottom: 2rem
+      .tags
+        max-width: 80%
+        flex-wrap: wrap
+        justify-content: center
+        margin-bottom: 1.5rem
       .close-btn
         position: absolute
         top: 1.5rem
@@ -156,6 +156,21 @@ const currentProduction: any = ref(null)
         cursor: pointer
         &:hover
           filter: $filter-highlight
+      .content
+        width: 100%
+        height: 100%
+        overflow-y: scroll
+        padding-right: 1rem
+        @include content-style
+      ::-webkit-scrollbar
+        width: 5px
+        height: 5px
+        background: rgba(0, 0, 0, 0)
+      ::-webkit-scrollbar-thumb
+        border-radius: 1rem
+        background: $color-highlight
+        &:active
+          background: $color-wrong
     .background
       z-index: 10
       position: fixed
@@ -165,10 +180,31 @@ const currentProduction: any = ref(null)
       width: 100%
       background-color: rgba(0, 30, 29, 0.75)
       backdrop-filter: blur(7.5px)
+  .tags
+    display: flex
+    gap: 0.5rem
+    li
+      display: flex
+      gap: 0.25rem
+      align-items: center
+      background-color: $color-secondary
+      padding: 0.25rem
+      border-radius: 0.25rem
+      img
+        filter: $filter-primary
+        width: 1rem
+        height: 1rem
+      span
+        color: $color-primary
+        font-size: 0.825rem
+        font-weight: 800
   @media (max-width: 2000px)
     padding: 7.5rem 5rem
   @media (max-width: $l)
     padding: 5rem
+    .reader .container
+      padding: 2rem 1.25rem
+      width: 95vw
   @media (max-width: $m)
     padding: 3.75rem
   @media (max-width: $xs)
